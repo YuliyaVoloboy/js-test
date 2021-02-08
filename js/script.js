bf26f03cb468d12f5474e48c87ba97f64f303b67
Yulia <yvoloboy@gmail.ru>
2021-02-08 11:36:18 +0300

    While Do-while For

--- js/script.js
+++ js/script.js
@@ -1,5 +1,71 @@
 "use strict";
 
+/*  While Do-while For */
+
+let num = 50;
+
+/*
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+*/
+/*
+do {
+    console.log(num);
+    num++;
+} 
+while (num < 55);
+*/
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
+
+
+/*  If-else Switch */
+/*
+const num = 50;
+
+if (num < 50) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok');
+}
+
+if(4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+(num == 50) ? console.log('Ok') : console.log('Error'); // тернарный оператор
+*/
+
+/*
+const num = 49;
+
+switch (num) {
+    case 49:
+        console.log('Неверно..');
+        break;
+    case 100:
+        console.log('Неверно..');
+        break;
+    case 50:
+        console.log('Вот теперь правильно!');
+        break;
+    default: 
+        console.log('Не в этот раз..');
+}
+*/
+
 let number = 5;
 const leftBorderWidth = 1;
 
